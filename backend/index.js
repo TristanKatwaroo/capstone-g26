cf596fcdd7b699157558a73987602d8e4494ab47
Nicholas Brown <brown371@sheridancollege.ca>
2025-11-11 16:21:30 -0500

    feat: created file upload endpoint

--- backend/index.js
+++ backend/index.js
@@ -3,14 +3,34 @@ const cors = require('cors');
 const app = express();
 const PORT = 8080;
 
+const multer = require('multer');
+
+
 // --- MIDDLEWARE ---
 app.use(cors());
 app.use(express.json());
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'uploads')
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname)
+  }
+});
+
+const upload = multer({storage});
+
 
 // --- ROUTES ---
 app.get('/api/test', (req, res) => {
   console.log("Received a request at /api/test");
   res.json({ message: "Hello from the Express backend! 👋" });
+});
+
+app.post("/api/upload", upload.single('file'), (req, res) => {
+  res.send("upload complete")
+  //res.json(req.file)
 });
 
 // --- START SERVER ---
