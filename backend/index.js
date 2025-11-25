5d8717d33e6d5091ce89fc49bdbc7faa4c48182f
Nicholas Brown <brown371@sheridancollege.ca>
2025-11-25 10:30:22 -0500

    created extractAudio and mergeAudioVide routes

--- backend/index.js
+++ backend/index.js
@@ -1,26 +1,27 @@
 const express = require('express');
 const cors = require('cors');
+const ffmpegService = require('./services/ffmpegService');
 const { transcribeAudio } = require('./services/transcriptionService');
 const app = express();
 const PORT = 8080;
 
-const multer = require('multer');
+// const multer = require('multer');
 
 
 // --- MIDDLEWARE ---
 app.use(cors());
 app.use(express.json());
 
-const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, 'uploads')
-  },
-  filename: function (req, file, cb) {
-    cb(null, file.originalname)
-  }
-});
+// const storage = multer.diskStorage({
+//   destination: function (req, file, cb) {
+//     cb(null, 'uploads')
+//   },
+//   filename: function (req, file, cb) {
+//     cb(null, file.originalname)
+//   }
+// });
 
-const upload = multer({storage});
+// const upload = multer({storage});
 
 
 // --- ROUTES ---
@@ -29,10 +30,22 @@ app.get('/api/test', (req, res) => {
   res.json({ message: "Hello from the Express backend! 👋" });
 });
 
-app.post("/api/upload", upload.single('file'), (req, res) => {
-  res.send("upload complete")
-  //res.json(req.file)
+app.post("/api/upload", ffmpegService.upload.single('file'), (req, res) => {
+  const filePath = req.file.path;
+  console.log("Request From Upload")
+  ffmpegService.extractAudio(filePath);
+  res.send("Upload Complete");
 });
+
+app.post("/api/extractAudio", (req, res) => {
+  ffmpegService.extractAudio("uploads/input.mp4");
+  res.send("Done?");
+})
+
+app.post('/api/mergeAudioVideo', (req, res) => {
+  ffmpegService.mergeAudioVideo("uploads/input.mp4", "test.mp3");
+  res.send("Complete");
+})
 
 // --- TEMPORARY TEST ROUTE ---
 // Usage: Make a POST request with a JSON body: { "filePath": "./sample.mp3" }
